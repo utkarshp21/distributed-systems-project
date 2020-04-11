4e97b0ba287671109a9c2e4b9d90a53aa5f9d29d
umar <swumar@gmail.com>
2020-04-11 18:34:37 +0530

    Refactoring login handler

--- src/auth/controller/controller.go
+++ src/auth/controller/controller.go
@@ -2,7 +2,7 @@ package controller
 
 import (
 	//authmodel "auth/model"
-	repository "auth/repository"
+	//repository "auth/repository"
 	service "auth/service"
 	//"container/list"
 	"log"
@@ -29,13 +29,12 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 			log.Println(errMsg)
 			t.Execute(w, m)
 			return
+		}else{
+			log.Println("User Registered succesfully")
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
 		}
-
-		log.Println("User Registered succesfully")
-		http.Redirect(w, r, "/login", http.StatusFound)
 	}
-	
-	return
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,46 +46,19 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		t.Execute(w, nil)
 		return 
-	}
+	}else{
 
-	r.ParseForm()
+		errMsg := service.LoginService(w,r)
 
-	user, usernameExists :=  repository.ReturnUser(r.Form["username"][0])
-
-	if usernameExists {
-
-		passowrdErr := repository.CheckLoginPassword(user.Password, r.Form["password"][0])
-		
-		if  passowrdErr != nil {
-			m["Error"] = "Invalid password"
-			log.Println("Invalid password")
-			t.Execute(w, m)
-			return			
-		}
-
-		tokenString, loginErr := repository.GenerateToken(user)
-
-		if loginErr != nil {
-			m["Error"] = "Error while generating token,Try again"
-			log.Println("Error while generating token,Try again")
+		if errMsg != "" {
+			m["Error"] = errMsg
+			log.Println(errMsg)
 			t.Execute(w, m)
 			return
+		}else{
+			log.Println("Login successful")
+			http.Redirect(w, r, "/profile", http.StatusFound)
+			return
 		}
-
-		http.SetCookie(w, &http.Cookie{
-			Name:    "token",
-			Value:   tokenString,
-		})
-
-		log.Println("Login successful")
-		http.Redirect(w, r, "/profile", http.StatusFound)
-
-		return
-
 	}
-
-	m["Error"] = "Invalid username"
-	log.Println("Invalid username")
-	t.Execute(w, m)
-	return	
 }
