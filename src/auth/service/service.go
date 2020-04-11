07d54ba157ccf6f5422d866840dfd533e182b09a
umar <swumar@gmail.com>
2020-04-11 18:34:19 +0530

    Refactoring login handler

--- src/auth/service/service.go
+++ src/auth/service/service.go
@@ -4,8 +4,6 @@ import (
 	authmodel "auth/model"
 	repository "auth/repository"
 	"container/list"
-	//"log"
-	//"html/template"
 	"net/http"
 )
 
@@ -34,4 +32,36 @@ func RegisterService (r *http.Request)(string){
 	}
 
 	return ""
+}
+
+func LoginService(w http.ResponseWriter ,r *http.Request)(string)  {
+
+
+	r.ParseForm()
+
+	user, usernameExists :=  repository.ReturnUser(r.Form["username"][0])
+
+	if usernameExists {
+
+		passowrdErr := repository.CheckLoginPassword(user.Password, r.Form["password"][0])
+
+		if  passowrdErr != nil {
+			return "Invalid password"
+		}
+
+		tokenString, loginErr := repository.GenerateToken(user)
+
+		if loginErr != nil {
+			return "Error while generating token,Try again"
+		}
+
+		http.SetCookie(w, &http.Cookie{
+			Name:    "token",
+			Value:   tokenString,
+		})
+
+		return ""
+
+	}
+	return "Invalid username"
 }
