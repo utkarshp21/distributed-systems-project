b6663472e68f619f0fbbf91a479daa1d00905fdf
umar <swumar@gmail.com>
2020-04-12 15:18:31 +0530

    Removing code from profile repository

--- src/profile/repository/repository.go
+++ src/profile/repository/repository.go
@@ -2,42 +2,9 @@ package repository
 
 import (
 	"container/list"
-	"fmt"
-	jwt "github.com/dgrijalva/jwt-go"
-	"net/http"
-	authmodel "auth/model"
-	authStorage "auth/storage"
 	profilemodel "profile/model"
 	profileStorage "profile/storage"
 )
-
-func GetToken(c *http.Cookie) (*jwt.Token,error) {
-	tokenString := c.Value
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method")
-		}
-		return []byte("secret"), nil
-	})
-	return token, err
-}
-
-func CheckIfUserAlreadyFollowed(followUser authmodel.User, userPresent authmodel.User)(bool){
-	for e := followUser.Followers.Front() ; e != nil ; e = e.Next(){
-		k := e.Value.(authmodel.User)
-		if userPresent == k{
-			return true
-		}
-	}
-	return false
-}
-
-func FollowUser(userPresent authmodel.User,followUser authmodel.User)  {
-	followUser.Followers.PushBack(userPresent)
-	authmodel.UsersMux.Lock()
-	authStorage.Users[followUser.Username] = followUser
-	authmodel.UsersMux.Unlock()
-}
 
 func SaveTweet(tweetUser string,tweetContent string){
 	profilemodel.TweetsMux.Lock()
