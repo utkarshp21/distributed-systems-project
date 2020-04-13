586eec5febb37a35ced58ea8f916bf7a46bdea86
umar <swumar@gmail.com>
2020-04-13 08:57:40 +0530

    Moving code from service to controller

--- src/profile/controller/controller.go
+++ src/profile/controller/controller.go
@@ -1,11 +1,12 @@
 package controller
 
 import (
+	"fmt"
 	//profileRepository "profile/repository"
 	//authRepository "auth/repository"
 	//"time"
 	"log"
-	//jwt "github.com/dgrijalva/jwt-go"
+	jwt "github.com/dgrijalva/jwt-go"
 	"html/template"
 	"net/http"
 	service "profile/service"
@@ -20,20 +21,38 @@ func redirectToLogin(w http.ResponseWriter){
 	t.Execute(w, m)
 }
 
+func GetToken(c *http.Cookie) (*jwt.Token,error) {
+	tokenString := c.Value
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method")
+		}
+		return []byte("secret"), nil
+	})
+	return token, err
+}
+
 func ProfileHandler(w http.ResponseWriter, r *http.Request) {
 	
 	t, _ := template.ParseFiles("profile.gtpl")
 
-	err := service.ProfileService(r)
+	c, err := r.Cookie("token")
 
 	if err != nil {
 		redirectToLogin(w)
 		return
-	}else{
+	}
+	token, tokenerr := GetToken(c)
+
+	if token.Valid && tokenerr == nil{
 		log.Println("Profile loaded succesfully")
 		t.Execute(w, nil)
 		return
+	}else{
+		redirectToLogin(w)
+		return
 	}
+
 }
 
 func FollowHandler(w http.ResponseWriter, r *http.Request) {
@@ -45,15 +64,30 @@ func FollowHandler(w http.ResponseWriter, r *http.Request) {
 	m := map[string]interface{}{}
 	t, _ := template.ParseFiles("profile.gtpl")
 
-	loginerr, err := service.FollowService(r)
+	c, err := r.Cookie("token")
 
-	if loginerr != nil {
+	if err != nil {
 		redirectToLogin(w)
 		return
-	}else if err != "" {
-		m["Error"] = err
+	}
+
+	token, tokenerr := GetToken(c)
+	if !token.Valid && tokenerr != nil{
+		redirectToLogin(w)
+		return
+	}
+
+	r.ParseForm()
+	claims, _ := token.Claims.(jwt.MapClaims)
+	userPresentUsername := r.Form["username"][0]
+	followuserUsername := claims["username"].(string)
+
+	srvErr := service.FollowService(userPresentUsername,followuserUsername)
+
+	if srvErr != "" {
+		m["Error"] = srvErr
 		m["Success"] = nil
-		log.Println(err)
+		log.Println(srvErr)
 		t.Execute(w, m)
 		return
 	}else {
@@ -74,15 +108,30 @@ func UnfollowHandler(w http.ResponseWriter, r *http.Request) {
 	m := map[string]interface{}{}
 	t, _ := template.ParseFiles("profile.gtpl")
 
-	loginerr, err := service.UnfollowService(r)
+	c, err := r.Cookie("token")
 
-	if loginerr != nil {
+	if err != nil {
 		redirectToLogin(w)
 		return
-	}else if err != "" {
-		m["Error"] = err
+	}
+
+	token, tokenerr := GetToken(c)
+	if !token.Valid && tokenerr != nil{
+		redirectToLogin(w)
+		return
+	}
+
+	r.ParseForm()
+	claims, _ := token.Claims.(jwt.MapClaims)
+	userPresentUsername := r.Form["username"][0]
+	unfollowuserUsername := claims["username"].(string)
+
+	srvErr := service.UnfollowService(userPresentUsername,unfollowuserUsername)
+
+	if srvErr != "" {
+		m["Error"] = srvErr
 		m["Success"] = nil
-		log.Println(err)
+		log.Println(srvErr)
 		t.Execute(w, m)
 		return
 	}else {
@@ -103,15 +152,30 @@ func TweetHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("profile.gtpl")
 	m := map[string]interface{}{}
 
-	loginerr, err := service.TweetService(r)
+	c, err := r.Cookie("token")
 
-	if loginerr != nil {
+	if err != nil {
 		redirectToLogin(w)
 		return
-	}else if err != "" {
-		m["Error"] = err
+	}
+
+	token, tokenerr := GetToken(c)
+	if !token.Valid && tokenerr != nil{
+		redirectToLogin(w)
+		return
+	}
+
+	r.ParseForm()
+	tweetContent := r.Form["tweet"][0]
+	claims, _ := token.Claims.(jwt.MapClaims)
+	tweetUserUsername := claims["username"].(string)
+
+	srvErr := service.TweetService(tweetContent,tweetUserUsername)
+
+	if srvErr != "" {
+		m["Error"] = srvErr
 		m["Success"] = nil
-		log.Println(err)
+		log.Println(srvErr)
 		t.Execute(w, m)
 		return
 	}else {
@@ -132,15 +196,28 @@ func FeedHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("profile.gtpl")
 	m := map[string]interface{}{}
 
-	loginerr, err, feed := service.FeedService(r)
+	c, err := r.Cookie("token")
 
-	if loginerr != nil {
+	if err != nil {
 		redirectToLogin(w)
 		return
-	}else if err != "" {
-		m["Error"] = err
+	}
+
+	token, tokenerr := GetToken(c)
+	if !token.Valid && tokenerr != nil{
+		redirectToLogin(w)
+		return
+	}
+
+	claims, _ := token.Claims.(jwt.MapClaims)
+	feedUserUsername := claims["username"].(string)
+
+	srvErr, feed := service.FeedService(feedUserUsername)
+
+	if srvErr != "" {
+		m["Error"] = srvErr
 		m["Success"] = nil
-		log.Println(err)
+		log.Println(srvErr)
 		t.Execute(w, m)
 		return
 	}else {
