51f66f5906527c4a19894b3fd18b2f4e9ba9ecd5
umar <swumar@gmail.com>
2020-05-01 09:06:51 +0530

    Creating web module and moving controller in it

--- src/web/controller/controller.go
+++ src/web/controller/controller.go
@@ -1,18 +1,167 @@
 package controller
 
 import (
-	"context"
 	"fmt"
-	"log"
-	jwt "github.com/dgrijalva/jwt-go"
 	"html/template"
 	"net/http"
-	"google.golang.org/grpc"
+	"time"
 	"auth/authpb"
+	"context"
+	"log"
+	"google.golang.org/grpc"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+
+	_, err := r.Cookie("token")
+
+	if err == nil {
+		http.Redirect(w, r, "/feed", http.StatusFound)
+		return
+	}
+
+	m := map[string]interface{}{}
+	t, _ := template.ParseFiles("../static/register.gtpl")
+
+	if r.Method == "GET" {
+		t.Execute(w, m)
+		return
+	} else {
+		var opts = grpc.WithInsecure()
+		var cc, ccerr = grpc.Dial("localhost:50051", opts)
+
+		if ccerr != nil {
+			log.Fatal(ccerr)
+		}
+
+		defer cc.Close()
+
+		client := authpb.NewRegisterServiceClient(cc)
+
+		r.ParseForm()
+		request := &authpb.RegisterRequest{Firstname: r.Form["firstname"][0], Lastname:r.Form["lastname"][0], Username:r.Form["username"][0], Password:r.Form["password"][0]}
+		response, _ := client.Register(context.Background(), request)
+
+		if response.Message != "" {
+			m["Error"] = response.Message
+			log.Println(response.Message)
+			t.Execute(w, m)
+			return
+		} else {
+			log.Println("User Registered succesfully")
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
+	}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+
+	t, _ := template.ParseFiles("../static/login.gtpl")
+
+	_, err := r.Cookie("token")
+
+	if err == nil {
+		http.Redirect(w, r, "/feed", http.StatusFound)
+		return
+	}
+
+	m := map[string]interface{}{}
+
+	if r.Method == "GET" {
+		t.Execute(w, nil)
+		return
+	} else {
+
+		var opts = grpc.WithInsecure()
+		var cc, ccerr = grpc.Dial("localhost:50051", opts)
+
+		if ccerr != nil {
+			log.Fatal(ccerr)
+		}
+
+		defer cc.Close()
+
+		client := authpb.NewLoginServiceClient(cc)
+
+		r.ParseForm()
+		request := &authpb.LoginRequest{Username: r.Form["username"][0], Password: r.Form["password"][0]}
+		response , _ := client.Login(context.Background(), request)
+
+		if response.Message != "" {
+			m["Error"] = response.Message
+			log.Println(response.Message)
+			t.Execute(w, m)
+			return
+		} else {
+			http.SetCookie(w, &http.Cookie{
+				Name:  "token",
+				Value: response.Tokenstring,
+			})
+			log.Println("Login successful")
+			http.Redirect(w, r, "/feed", http.StatusFound)
+			return
+		}
+	}
+}
+
+func SignoutHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "GET" {
+		http.Redirect(w, r, "/feed", http.StatusFound)
+		return
+	}
+
+	t, _ := template.ParseFiles("../static/login.gtpl")
+	m := map[string]interface{}{}
+
+	c, cerr := r.Cookie("token")
+	if cerr != nil {
+		m["Error"] = "Please login to continue"
+		m["Success"] = nil
+		log.Println("Please login to continue")
+		t.Execute(w, m)
+		return
+	}
+
+	tokenString := c.Value
+
+	var opts = grpc.WithInsecure()
+	var cc, ccerr = grpc.Dial("localhost:50051", opts)
+
+	if ccerr != nil {
+		log.Fatal(ccerr)
+	}
+
+	defer cc.Close()
+
+	client := authpb.NewLogoutServiceClient(cc)
+
+	request := &authpb.LogoutRequest{Tokenstring: tokenString}
+
+	response, _ := client.Logout(context.Background(), request)
+
+	if response.Message != "" {
+		m["Error"] = response.Message
+		m["Success"] = nil
+		log.Println(response.Message)
+		t.Execute(w, m)
+		return
+	} else {
+		http.SetCookie(w, &http.Cookie{
+			Name:    "token",
+			Value:   "",
+			Expires: time.Unix(0, 0),
+		})
+		log.Println("Logout successful")
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+}
+
 func redirectToLogin(w http.ResponseWriter){
-	t, _ := template.ParseFiles("login.gtpl")
+	t, _ := template.ParseFiles("../static/login.gtpl")
 	m := map[string]interface{}{}
 	m["Error"] = "Please login to continue!"
 	m["Success"] = nil
@@ -32,7 +181,7 @@ func GetToken(c *http.Cookie) (*jwt.Token,error) {
 }
 
 func UserListHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("userlist.gtpl")
+	t, _ := template.ParseFiles("../static/userlist.gtpl")
 	m := map[string]interface{}{}
 
 	c, err := r.Cookie("token")
@@ -89,7 +238,7 @@ func FollowHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	m := map[string]interface{}{}
-	t, _ := template.ParseFiles("userlist.gtpl")
+	t, _ := template.ParseFiles("../static/userlist.gtpl")
 
 	c, err := r.Cookie("token")
 
@@ -145,7 +294,7 @@ func UnfollowHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	m := map[string]interface{}{}
-	t, _ := template.ParseFiles("userlist.gtpl")
+	t, _ := template.ParseFiles("../static/userlist.gtpl")
 
 	c, err := r.Cookie("token")
 
@@ -200,7 +349,7 @@ func TweetHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	t, _ := template.ParseFiles("profile.gtpl")
+	t, _ := template.ParseFiles("../static/profile.gtpl")
 	m := map[string]interface{}{}
 
 	c, err := r.Cookie("token")
@@ -252,7 +401,7 @@ func TweetHandler(w http.ResponseWriter, r *http.Request) {
 
 func FeedHandler(w http.ResponseWriter, r *http.Request) {
 
-	t, _ := template.ParseFiles("profile.gtpl")
+	t, _ := template.ParseFiles("../static/profile.gtpl")
 	m := map[string]interface{}{}
 
 	c, err := r.Cookie("token")
---
+++ src/web/main/web.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	//authController "auth/controller"
+	"log"
+	"net/http"
+	//profileController "profile/controller"
+	"github.com/gorilla/mux"
+	controller "web/controller"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", controller.LoginHandler)
+	r.HandleFunc("/register", controller.RegisterHandler)
+	r.HandleFunc("/login", controller.LoginHandler)
+	r.HandleFunc("/unfollow", controller.UnfollowHandler)
+	r.HandleFunc("/follow", controller.FollowHandler)
+	r.HandleFunc("/tweet", controller.TweetHandler)
+	r.HandleFunc("/feed", controller.FeedHandler)
+	r.HandleFunc("/signout", controller.SignoutHandler)
+	r.HandleFunc("/userlist", controller.UserListHandler)
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
